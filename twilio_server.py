da59ca2d0e619e8e324d8cae63d4728d6667ffb3
Aaron Rosen <aaronorosen@gmail.com>
2014-08-19 19:02:51 -0700

    Updated message back

--- twilio_server.py
+++ twilio_server.py
@@ -32,7 +32,9 @@ def answer_call():
     db.session.add(call_entry)
     db.session.commit()
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    resp.say("This call is being served on from docker container "
+             + str(socket.gethostname()) + " running on VMWare NSX. The"
+             " rest of the week should be sunny in san francisco.")
 
     return str(resp)
 
